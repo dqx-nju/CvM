38c9fd4d1010202b847dd827394a909e1f564597
shuifengche <shuifengche@foxmail.com>
2020-12-31 16:32:12 +0800

    save_file bug

--- src/main/java/org/cvm/output/PlayFile.java
+++ src/main/java/org/cvm/output/PlayFile.java
@@ -1,9 +1,6 @@
 package org.cvm.output;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,23 +12,14 @@ public class PlayFile {
     }
 
     public void save_file(String filepath) {
-        FileWriter fw = null;
+
+        PrintStream stream = null;
         try {
-            File f = new File(filepath);
-            fw = new FileWriter(f, true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        PrintWriter pw = new PrintWriter(fw);
-        for (int i = 0; i < list.size(); i++) {
-            pw.println(list.get(i));
-        }
-        pw.flush();
-        try {
-            fw.flush();
-            pw.close();
-            fw.close();
-        } catch (IOException e) {
+            stream = new PrintStream(filepath);
+            for (int i = 0; i < list.size(); i++) {
+                stream.println(list.get(i));
+            }
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
